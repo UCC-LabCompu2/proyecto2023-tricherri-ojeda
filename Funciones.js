d9ca74e0ef22d892819c06533d78d6f1e2fa672a
lautiod <dantelojeda05@gmail.com>
2023-05-30 20:20:01 -0300

    Se hizo Botón de Copiar

--- Funciones.js
+++ Funciones.js
@@ -67,3 +67,37 @@ function textoAMorse() {
     // Muestra el resultado en el span con el id "resultado"
     document.getElementById("resultado").textContent = resultado;
 }
+/**
+ * Descripción de que hace la función
+ * @method Nombre de la función
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+ * @return Valor que retorna
+ */
+function copiarContenidoSpan() {
+    // Obtener el elemento <span> por su id
+    var span = document.getElementById('resultado');
+
+    // Crear un elemento temporal para almacenar el contenido
+    var tempElement = document.createElement('textarea');
+    tempElement.value = span.textContent;
+
+    // Añadir el elemento temporal al documento
+    document.body.appendChild(tempElement);
+
+    // Seleccionar el contenido del elemento temporal
+    tempElement.select();
+    tempElement.setSelectionRange(0, 99999); // Para dispositivos móviles
+
+    // Copiar el contenido al portapapeles utilizando el API del portapapeles
+    navigator.clipboard.writeText(tempElement.value)
+        .then(function() {
+            console.log('Contenido copiado al portapapeles');
+        })
+        .catch(function(error) {
+            console.error('Error al copiar el contenido: ', error);
+        });
+
+    // Remover el elemento temporal del documento
+    document.body.removeChild(tempElement);
+}
