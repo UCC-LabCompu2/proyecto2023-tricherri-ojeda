1687ea39ffcbd4f07c80c7c02d804eadf4ac867d
lautiod <dantelojeda05@gmail.com>
2023-06-05 23:52:45 -0300

    Se documento funcion dibujar

--- Funciones.js
+++ Funciones.js
@@ -153,7 +153,10 @@ let borrarCanvas = () =>{
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-
+/**
+ * Dibuja las ondas sobre el canvas
+ * @method dibujarOndas
+ */
 let dibujarOndas = () => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
