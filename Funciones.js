b5d0b7ab90af7e94c50ee514ecb69d4d572dbd82
lautiod <dantelojeda05@gmail.com>
2023-06-05 23:37:46 -0300

    Se agrego funcion para borrar canvas

--- Funciones.js
+++ Funciones.js
@@ -143,3 +143,13 @@ let reproducir = () => {
     oscilador.start();
     oscilador.stop(tiempoActual);
 }
+/**
+ * Borra el contenido del canvas
+ * @method borrarCanvas
+ */
+let borrarCanvas = () =>{
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
